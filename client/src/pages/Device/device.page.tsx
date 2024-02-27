eaa50931326bd4ae164c6c0946c2c30edb656c36
Joel Campos <joelcamposoliva9000@gmail.com>
2024-02-27 16:06:14 +0100

    Add map

--- client/src/pages/Device/device.page.tsx
+++ client/src/pages/Device/device.page.tsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 import styles from './device.module.css';
 import axios from 'axios';
+import { MapContainer } from 'react-leaflet/MapContainer';
+import { TileLayer } from 'react-leaflet/TileLayer';
 
 export default function Device() {
   
@@ -9,7 +11,6 @@ export default function Device() {
   const device = {name: `Device ${id}`, description: 'Random description', id, online: true, type: 'Mobile'};
 
     return (
-    <>
       <div className={styles.device}>
         <h2>{device.name}</h2>
         <p>{device.description}</p>
@@ -19,7 +20,13 @@ export default function Device() {
         <h5>Modelo: Nokia</h5>
         <h5>Nombre de modelo</h5>
         <h5>IMEI</h5>
+
+        <MapContainer style={{ height: '25em'}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
       </div>
-    </>
     )   
 }

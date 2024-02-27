925df96339ccc40eb1f144e49f22bcaf46f3bf6b
Joel Campos <joelcamposoliva9000@gmail.com>
2024-02-27 16:32:50 +0100

    Map pointer

--- client/src/components/Map/device-map.tsx
+++ client/src/components/Map/device-map.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Popup, TileLayer } from "react-leaflet";
 import { DeviceLocationModel } from "../../models/location/device-location.model";
 import { Skeleton } from "antd";
 import styles from './device-map.module.css';
@@ -15,6 +15,10 @@ export default function DeviceMap({ location }: Props) {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url={`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`}
               />
+              <Popup position={{
+                lat: location.latitude,
+                lng: location.longitude,
+              }}>Device is here</Popup>
             </MapContainer>
           ) : (<Skeleton/>)}
     </div>

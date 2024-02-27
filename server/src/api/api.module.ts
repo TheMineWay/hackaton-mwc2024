4a6d23d46c30d4135df6f62f152fa6ed7f4a8d81
Joel Campos <joelcamposoliva9000@gmail.com>
2024-02-27 13:51:34 +0100

    Add devices module

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { LocationModule } from './location/location.module';
+import { DevicesModule } from './devices/devices.module';
 
 @Module({
-  imports: [LocationModule],
+  imports: [LocationModule, DevicesModule],
 })
 export class ApiModule {}

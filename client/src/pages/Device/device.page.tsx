47ab54bfe1c980de88152683e852d23ffefa8817
luciano-mc <luciano.martiin@gmail.com>
2024-02-27 17:44:39 +0100

    add connect/disconnect on device

--- client/src/pages/Device/device.page.tsx
+++ client/src/pages/Device/device.page.tsx
@@ -1,10 +1,11 @@
 import { useParams } from 'react-router-dom';
-import { MapContainer } from 'react-leaflet/MapContainer';
-import { TileLayer } from 'react-leaflet/TileLayer';
 import { useDeviceById } from '../../hooks/devices/use-device-by-id';
 import { Skeleton } from 'antd';
+import { useDeviceLocation } from '../../hooks/location/use-device-location';
+import DeviceMap from '../../components/Map/device-map';
 import styles from './device.module.css';
-import { ApiOutlined, EditOutlined, PhoneFilled } from '@ant-design/icons';
+import { ApiOutlined, DisconnectOutlined, EditOutlined, MenuOutlined, PhoneFilled } from '@ant-design/icons';
+import { useDeviceStatus } from '../../hooks/status/use-device-status';
 
 export default function Device() {
   
@@ -12,38 +13,43 @@ export default function Device() {
   if (!id) throw new Error();
 
   const { data: device } = useDeviceById(id);
+  const { data: location } = useDeviceLocation(id);
 
-  const online = false;
-
+  const { isOnline, setOnlineStatus } = useDeviceStatus();
+  
   if (!device) return <Skeleton paragraph/>;
 
   return (      
     <div className={styles.device}>
-      <div className="title">
       <h2>{device.name}</h2>
-
-      </div>
       <p>{device.description}</p>
-      <h3>Status: {online ? '🟢 online' : '🔴 offline'}</h3>
-      <h5>Dispositivo: {device.type}</h5>
-      <h5>Marca: Nokia</h5>
-      <h5>Modelo: Nokia</h5>
-      <h5>Nombre de modelo</h5>
-      <h5>IMEI</h5>
-      <h5>Conectado a: PC-Casa</h5>
-      <h5>Pais: España</h5>
-
-      <MapContainer style={{ height: '25em'}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
+      <h3>Status: {isOnline ? '🟢 online' : '🔴 offline'}</h3>
+      <div className={styles.device__info}>
+        <div className="device__info__col">
+          <h5>Dispositivo: {device.type}</h5>
+          <h5>Marca: Nokia</h5>
+          <h5>Modelo: Nokia</h5>
+          <h5>Nombre de modelo</h5>
+        </div>
+        <div className="device__info__col">
+          <h5>IMEI</h5>
+          <h5>Conectado a: PC-Casa</h5>
+          <h5>Pais: España</h5>
+        </div>
+      </div>
       
       <div className={styles.icons}>
         <PhoneFilled title="Videocall" />
         <EditOutlined title="Edit" />
-        <ApiOutlined title="Disconnect" />
+        { isOnline ? 
+          <ApiOutlined title="Connect" onClick={() => setOnlineStatus(!isOnline)} />
+        : 
+          <DisconnectOutlined title="Disconnect" onClick={() => setOnlineStatus(!isOnline)} />
+        }
+        <MenuOutlined title="Options" />
+      </div>
+      <div>
+        <DeviceMap location={location}/>
       </div>
     </div>
   );

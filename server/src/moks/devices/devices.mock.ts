e2116caef86e32918d4c69e9cfbff1bc584a0485
Joel Campos <joelcamposoliva9000@gmail.com>
2024-02-27 14:50:39 +0100

    Change devices mock

--- server/src/moks/devices/devices.mock.ts
+++ server/src/moks/devices/devices.mock.ts
@@ -1,3 +1,14 @@
 export const DEVICES_MOCK = [
-  '549f5eb2-96ca-4bfa-a52f-32efc7467973@testcsp.net',
+  {
+    id: 'joel1@testcsp.net',
+    name: 'Test1',
+  },
+  {
+    id: 'joel2@testcsp.net',
+    name: 'Test2',
+  },
+  {
+    id: 'joel3@testcsp.net',
+    name: 'Test3',
+  },
 ];

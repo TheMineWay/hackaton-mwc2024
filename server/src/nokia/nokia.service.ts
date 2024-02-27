4584abd4fb50666fd60f13bc5b1f5ed5b8edbcc7
Joel Campos <joelcamposoliva9000@gmail.com>
2024-02-27 12:24:44 +0100

    Init nokia

--- server/src/nokia/nokia.service.ts
+++ server/src/nokia/nokia.service.ts
@@ -1,15 +1,42 @@
 import { ENV } from '@constants/env.constant';
+import { NOKIA } from '@constants/nokia.constant';
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosRequestConfig } from 'axios';
 
 @Injectable()
 export class NokiaService {
-  readonly nokiaApiKey: string;
+  readonly apiKey: string;
+  readonly apiHost = NOKIA.apiHost;
 
   constructor() {
-    this.nokiaApiKey = ENV.NOKIA_API_KEY;
+    this.apiKey = ENV.NOKIA_API_KEY;
   }
 
   async getDeviceLocationByDeviceId(deviceId: string) {
-    return deviceId;
+    return await this.request('retrieve', {
+      method: 'post',
+      data: {
+        device: {
+          networkAccessIdentifier: {},
+        },
+        maxAge: {},
+      },
+    });
+  }
+
+  private async request<T extends object = object>(
+    path: string,
+    config: Omit<AxiosRequestConfig<T>, 'url'>,
+  ) {
+    return this.apiKey;
+    return await axios.request({
+      ...config,
+      url: `${this.apiHost}/${path}`,
+      headers: {
+        ...(config.headers ?? {}),
+        'X-RapidAPI-Key': this.apiKey,
+        'X-RapidAPI-Host': 'location-retrieval.nokia.rapidapi.com',
+      },
+    });
   }
 }

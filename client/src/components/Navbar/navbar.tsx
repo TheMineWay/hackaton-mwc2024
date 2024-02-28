a56b6c790360d0b436ef967ef6fca524815ad6e9
luciano-mc <luciano.martiin@gmail.com>
2024-02-28 08:07:32 +0100

    update logo

--- client/src/components/Navbar/navbar.tsx
+++ client/src/components/Navbar/navbar.tsx
@@ -1,14 +1,16 @@
 import { Avatar } from 'antd';
-import { UserOutlined, InboxOutlined, BellOutlined, MoonFilled } from '@ant-design/icons';
+import Logo from './navbarLogo.png';
+import { UserOutlined, InboxOutlined, BellOutlined } from '@ant-design/icons';
 import './navbar.css';
 
 function Navbar() {
   return (
     <>
       <div className="navbar">
-        <div className="logo__container">
-          <MoonFilled />
-          <span>Moon</span>
+        <div className="logo_content">
+          <div className="logo__container">
+            <img src={Logo} alt="Logo" className="logo" />
+          </div>
         </div>
         <div className="user__container">
         <BellOutlined />

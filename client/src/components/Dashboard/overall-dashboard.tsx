15b0c527f5ccbe39355a05b3a296318f0798608e
Joel Campos <joelcamposoliva9000@gmail.com>
2024-02-28 13:30:33 +0100

    Chart

--- client/src/components/Dashboard/overall-dashboard.tsx
+++ client/src/components/Dashboard/overall-dashboard.tsx
@@ -3,6 +3,7 @@ import { useAllDevicesInfo } from '../../hooks/devices/use-all-devices-info';
 import DeviceMap from '../Map/device-map';
 import DashboardTable from '../Table/dashboardTable';
 import styles from './overall-dashboard.module.css';
+import BandwidthChart from '../Charts/bandwidth-chart';
 
 export function OverallDashboard() {
 
@@ -24,6 +25,7 @@ export function OverallDashboard() {
         <DashboardTable />
         <Row>
             <Col xs={24} md={12}>
+                <BandwidthChart/>
             </Col>
             <Col xs={24} md={12}>
                 <DeviceMap className={styles.map} location={sumLoc} pointers={data.map((l) => ({
